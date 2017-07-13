f8ccbda2b967417471f8a6aa72b80dc7b27b7819
wangning <2556405022@qq.com>
2017-07-13 22:24:37 +0800

    first

--- app/src/main/java/com/example/a25564/weifusifeng/MainActivity.java
+++ app/src/main/java/com/example/a25564/weifusifeng/MainActivity.java
@@ -2,27 +2,15 @@ package com.example.a25564.weifusifeng;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import com.example.a25564.weifusifeng.bean.user;
-
-import org.litepal.crud.DataSupport;
 
 public class MainActivity extends AppCompatActivity {
-    Button temp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        temp= (Button) findViewById(R.id.temp);
-        temp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DataSupport.deleteAll(user.class);
 
-            }
-        });
+
 
     }
 }
---
+++ app/src/main/java/com/example/a25564/weifusifeng/activity/BaseFragmentActivity.java
@@ -0,0 +1,12 @@
+package com.example.a25564.weifusifeng.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class BaseFragmentActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
